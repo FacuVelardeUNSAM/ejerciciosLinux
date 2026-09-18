4457dcfc1cee7e9f389dd94cf0228ce91a2b4e3f
facu <fuvelarde@estudiantes.unsam.edu.ar>
2026-09-17 21:29:21 -0300

    agrego comentarios

--- ej4.sh
+++ ej4.sh
@@ -1,3 +1,6 @@
-#! /bin/bash
+#!/bin/bash
 
-cut -d'"' -f 4 guid_to_fix.csv | sed '1d' 1> guid.csv
+cut -d'"' -f4 guid_to_fix.csv | sed '1d' > guid.csv
+
+# cut -d'"' -f4 guid_to_fix.csv | tail -n +2 1> guid.csv
+# con \ se puede hacer salto de linea para hacer pipes en varias lineas.
